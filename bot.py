70a73cfc2c12bb3f1f455f529f0bb504e7d36854
Alex Ng <avon1462@gmail.com>
2022-08-13 18:28:50 -0700

    hello world discord bot

--- bot.py
+++ bot.py
@@ -1,3 +1,29 @@
 # bot.py
+import discord
+from dotenv import load_dotenv
 import os
 
+# intents = discord.Intents.default()
+# intents.message_content = True
+
+load_dotenv('dev.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+    if 'short' in message.content:
+        await message.channel.send("Emily is short!")
+
+client.run(TOKEN)
