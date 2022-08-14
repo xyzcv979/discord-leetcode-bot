d109dcce93f4fb3d9598364e7d4f8a44dd45e166
Alex Ng <avon1462@gmail.com>
2022-08-13 19:29:39 -0700

    Added on member join channel message

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # bot.py
+from email import message
 import discord
 from dotenv import load_dotenv
 import os
@@ -9,21 +10,25 @@ import os
 load_dotenv('dev.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = discord.Client() # connection to discord
 
-@client.event
-async def on_ready():
+@client.event # registers event
+async def on_ready(): # once bot is online
     print(f'We have logged in as {client.user}')
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(message): # on receiving a message
+    if message.author == client.user: # doesn't do anything is message is from bot
         return
     
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
     
-    if 'short' in message.content:
+    if 'emily' in message.content or 'Emily' in message.content:
         await message.channel.send("Emily is short!")
+
+@client.event
+async def on_member_join(member):
+    await message.channel.send(f'Hi {member.name}, welcome to {member.guild.name}')
 
 client.run(TOKEN)
