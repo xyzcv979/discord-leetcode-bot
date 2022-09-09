47cdec985861a518c6f3f024052a61819342203a
Alex Ng <avon1462@gmail.com>
2022-09-09 00:40:22 -0400

    Added scheduler

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ import discord
 from dotenv import load_dotenv
 import os
 from leetcode_requests import *
+import datetime
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,6 +23,8 @@ LEETCODE_CHANNEL = 1008072276396216342
 @bot.event # registers event
 async def on_ready(): # once bot is online
     print(f'{bot.user} connected to Discord!')
+    if not run_job.is_running():
+        run_job.start()
 
 # @bot.event
 # async def on_message(message): # on receiving a message
@@ -33,6 +36,15 @@ async def on_member_join(member):
     channel = bot.get_channel(GENERAL_CHANNEL) # general channel ID
     await channel.send(f'Hi {member.name}, welcome to {member.guild.name}')
 
+daily_time = datetime.time(hour=14, minute=0, second=0)
+@tasks.loop(time=daily_time)
+async def run_job():
+    await bot.wait_until_ready()
+    daily_url = get_leetcode_daily()
+    channel = bot.get_channel(LEETCODE_CHANNEL)
+    await channel.send(f'Daily problem: {daily_url}')
+
+
 @bot.command(name='daily', help='Responds with the daily leetcode problem')
 async def leetcode_daily(ctx): #ctx = context which holds channel/guild info
     await ctx.send(get_leetcode_daily())
@@ -41,53 +53,54 @@ async def leetcode_daily(ctx): #ctx = context which holds channel/guild info
 async def leetcode_random(ctx):
     await ctx.send(get_random_question())
 
-@bot.command(name='random_easy', help='random easy leetcode problem')
+@bot.command(name='random easy', help='random easy leetcode problem')
 async def leetcode_random_easy(ctx):
     await ctx.send(get_random_difficulty("EASY"))
 
-@bot.command(name='random_medium', help='random medium leetcode problem')
+@bot.command(name='random medium', help='random medium leetcode problem')
 async def leetcode_random_medium(ctx):
     await ctx.send(get_random_difficulty("MEDIUM"))
     
-@bot.command(name='random_hard', help='random hard leetcode problem')
+@bot.command(name='random hard', help='random hard leetcode problem')
 async def leetcode_random_hard(ctx):
     await ctx.send(get_random_difficulty("HARD"))
 
 
-@bot.command(name='random_array', help='random array leetcode problem')
+@bot.command(name='random array', help='random array leetcode problem')
 async def leetcode_random_array(ctx):
     await ctx.send(get_random_tag("array"))
 
-@bot.command(name='random_string', help='random string leetcode problem')
+@bot.command(name='random string', help='random string leetcode problem')
 async def leetcode_random_string(ctx):
     await ctx.send(get_random_tag("string"))
 
-@bot.command(name='random_dp', help='random dynamic-programming leetcode problem')
+@bot.command(name='random dp', help='random dynamic-programming leetcode problem')
 async def leetcode_random_dp(ctx):
     await ctx.send(get_random_tag("dynamic-programming"))
 
-@bot.command(name='random_dfs', help='random depth-first-search leetcode problem')
+@bot.command(name='random dfs', help='random depth-first-search leetcode problem')
 async def leetcode_random_dfs(ctx):
     await ctx.send(get_random_tag("depth-first-search"))
 
-@bot.command(name='random_bfs', help='random breadth-first-search leetcode problem')
+@bot.command(name='random bfs', help='random breadth-first-search leetcode problem')
 async def leetcode_random_bfs(ctx):
     await ctx.send(get_random_tag("breadth-first-search"))
 
-@bot.command(name='random_tree', help='random tree leetcode problem')
+@bot.command(name='random tree', help='random tree leetcode problem')
 async def leetcode_random_tree(ctx):
     await ctx.send(get_random_tag("tree"))
 
-@bot.command(name='random_graph', help='random graph leetcode problem')
+@bot.command(name='random graph', help='random graph leetcode problem')
 async def leetcode_random_graph(ctx):
     await ctx.send(get_random_tag("graph"))
 
-@bot.command(name='random_backtracking', help='random backtracking leetcode problem')
+@bot.command(name='random backtracking', help='random backtracking leetcode problem')
 async def leetcode_random_backtracking(ctx):
     await ctx.send(get_random_tag("backtracking"))
 
-@bot.command(name='random_linkedlist', help='random linked-list leetcode problem')
+@bot.command(name='random linkedlist', help='random linked-list leetcode problem')
 async def leetcode_random_linkedlist(ctx):
     await ctx.send(get_random_tag("linked-list"))
+
 
 bot.run(TOKEN)
